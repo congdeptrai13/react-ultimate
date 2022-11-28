2de62f1cca24a704f99edba78787ace447841ea5
code with congdeptrai <cong102.201@gmail.com>
2022-11-28 21:50:16 +0700

    video 20

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -9,18 +9,21 @@ class MyComponent extends React.Component {
     age: 20
   };
   handleClick(event) {
-    // console.log(">> click me my button");
-    
+    this.setState({
+      name: "eric",
+      age: Math.floor((Math.random() * 100) + 1),
+    })
   }
   handleOnMouseOver(event) {
-    console.log(event.pageX)
+    // console.log(event.pageX)
   }
   //JSX 
   render() {
     return (
       <div>
-        my name is {this.state.name} and i from {this.state.address}
+        my name is {this.state.name} and i {this.state.age}
         <button onMouseOver={this.handleOnMouseOver}>hover me</button>
+        <button onClick={(event) => { this.handleClick(event) }}>Click me</button>
       </div >
     );
   }
