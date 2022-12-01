97269c643e8e992a71bcb762586331109b06af5b
code with congdeptrai <cong102.201@gmail.com>
2022-12-01 16:57:59 +0700

    video 35

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,34 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import "./DisplayInfor.scss";
-import logo from "./../logo.svg";
-class DisplayInfor extends React.Component {
-  constructor(props) {
-    super(props);
-    //babel compiler
-    state = {
-      isShowListUser: true,
-    }
+//stateless vs stateful
+// class DisplayInfor extends React.Component {
+//   render() {
+//     console.log("call me render")
+//     //destructuring array/object
+//     const { listUsers } = this.props; //object
+//     //const listUsers = this.props.listUsers;
+//     //props => viết tắt properties
+//     //template + logic js
+//     return (
 
+//       < div className="display-infor-container" >
+//         {
+//           true &&
+//           <>
+//             {listUsers.map((user) => {
+//               return (
+//                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+//                   <div>my name's {user.name}</div>
+//                   <div>my age's {user.age}</div>
+//                   <div>
+//                     <button onClick={() => { this.props.handleDeleteUser(user.id) }}>Delete</button>
+//                   </div>
+//                   <hr />
+//                 </div>
+
+//               )
+//             })}
+
+//           </>
+//         }
+//       </div >
+
+//     )
+//   }
+// }
+
+
+const DisplayInfor = (props) => {
+  const { listUsers } = props; //object
+  const [isShowHideListUser, setShowHideListUser] = useState(true);
+  //destructuring assignment
+  const handleShowHideListUser = () => {
+    setShowHideListUser(!isShowHideListUser);
   }
-}
-handleShowHide = () => {
-  this.setState({
-    isShowListUser: !this.state.isShowListUser,
-  })
-}
-render() {
-  //destructuring array/object
-  const { listUsers } = this.props; //object
-  //const listUsers = this.props.listUsers;
-  //props => viết tắt properties
-  //template + logic js
   return (
-    <div className="display-infor-container">
-      {/* <img src={logo} /> */}
+    < div className="display-infor-container" >
       <div>
-        <span onClick={() => { this.handleShowHide() }}>{this.state.isShowListUser ? "hide" : "show"} list users:</span>
+        <span onClick={() => {
+          handleShowHideListUser()
+        }}>{isShowHideListUser ? "hide list user" : "show list user"}</span>
       </div>
-      {this.state.isShowListUser &&
+      {
+        isShowHideListUser &&
         <>
           {listUsers.map((user) => {
             return (
@@ -36,7 +61,7 @@ render() {
                 <div>my name's {user.name}</div>
                 <div>my age's {user.age}</div>
                 <div>
-                  <button onClick={() => { this.props.handleDeleteUser(user.id) }}>Delete</button>
+                  <button onClick={() => { props.handleDeleteUser(user.id) }}>Delete</button>
                 </div>
                 <hr />
               </div>
@@ -46,9 +71,7 @@ render() {
 
         </>
       }
-    </div>
-
+    </div >
   )
-}
 }
 export default DisplayInfor;
